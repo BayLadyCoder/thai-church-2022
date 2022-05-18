e7f7b5ee39a3d47bb869fd4f29b85e431bd103e2
BayLadyCoder <rachada_32@hotmail.com>
2022-05-18 00:47:24 -0400

    Use local photos for events

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,5 +1,8 @@
 import { Typography, Box, Button } from "@mui/material";
 import Event from "./Event";
+import OutdoorWorshipOct2021 from "../media/events/2021/10-03-2021.jpeg";
+import NorthernThaiChristmasImg from "../media/events/2021/12-19-2021.jpeg";
+import Easter2022Img from "../media/events/2022/04-17-2022.jpeg";
 
 const Events = (props) => {
   const events = [
@@ -7,8 +10,7 @@ const Events = (props) => {
       title: "Thai Church DC Easter",
       dateTime: "SUNDAY, APRIL 17, 2022 AT 11:30 PM",
       location: "Thai Church DC -  3910 Lorcom Lane Arlington, VA 22207.",
-      image:
-        "https://scontent.fagc1-2.fna.fbcdn.net/v/t39.30808-6/277001342_10227107904191954_2698984566983476786_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=340051&_nc_ohc=26RiyWkXRCsAX8Ldgeg&_nc_ht=scontent.fagc1-2.fna&oh=00_AT-VfoUzZzhcTprAaYR7KoopfIZWJ_8_5I8lo8j_DACueg&oe=6245B2F0",
+      image: Easter2022Img,
       link: "https://fb.me/e/5eiIhBb8r",
       detail:
         "Join us as we celebrate Easter on Sunday, April 17 at 11:30 am. We will have egg hunt for the children and potluck lunch after the service.",
@@ -18,8 +20,7 @@ const Events = (props) => {
       dateTime: "SUNDAY, DECEMBER 19, 2021 AT 11:30 AM",
       location: "Thai Church DC -  3910 Lorcom Lane Arlington, VA 22207.",
 
-      image:
-        "https://scontent.fagc1-1.fna.fbcdn.net/v/t39.30808-6/261683023_10226536558988681_3104353810534978662_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=340051&_nc_ohc=jwaAJnHVwDYAX-CkxY_&_nc_ht=scontent.fagc1-1.fna&oh=00_AT8gsmHKT8l36OhOKkmeafy-Xb5JEyWbM8o8OQAy5An9Xw&oe=62461751",
+      image: NorthernThaiChristmasImg,
       link: "https://fb.me/e/4iKHhnNju",
       detail:
         "คริสต์มาสก่อนโควิดเราจัดคริสต์มาสบ้านข่อย ปีนี้คริสตจักรไทยดีซีขอเชิญทุกท่านมาร่วมงานคริสต์มาสคนเมือง พระเจ้าทรงเป็นพระเจ้าของคนทุกชาติ ทุกเผ่าพันธุ์ เราจะมีรำชาวเหนือ อาหารเหนือ ข้าวนึ่ง ไส้อั่ว น้ำพริกอ่อง ข้าวซอยไก่ ขนมจีนน้ำเงี้ยว แหนมกระดูกหมู เทศนาโดยอาจารย์คนเมือง ใส่ชุดเหนือมาสนุกๆกัน เรามีของขวัญให้เด็กๆทุกคนค่า You are invited to Northern Thai Christmas. Couple years ago, we had Northeast Thai Christmas. We will have Northern Thai dance, Northern Thai food and preacher from Northern Thailand. We will have gift for all children. Dress Northern Thai just for fun.",
@@ -29,8 +30,7 @@ const Events = (props) => {
       dateTime: "SUNDAY, OCTOBER 3, 2021 AT 11 AM",
       location: "Lady Baines Johnson Memorial Grove",
 
-      image:
-        "https://scontent.fagc1-2.fna.fbcdn.net/v/t39.30808-6/240389407_10226162968649156_1578835197848198214_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=340051&_nc_ohc=l3DpCVCfxU8AX-oXdr2&_nc_ht=scontent.fagc1-2.fna&oh=00_AT8rBqpVLrV6yo_00_e0Y8q1JBm91q3KCeTf-mIQ7FFuwA&oe=62460B12",
+      image: OutdoorWorshipOct2021,
       link: "https://fb.me/e/2wyccBrH4",
       detail:
         "Come join Thai Church DC monthly worship and fellowship. Bring a lawn chair and picnic lunch.",
